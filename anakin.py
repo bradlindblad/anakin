a5388af257a16c173220542dad92c56258da10e0
bradlindblad <bradley.lindblad@gmail.com>
2018-07-10 09:44:50 -0500

    first few things

--- anakin.py
+++ anakin.py
@@ -3,7 +3,18 @@
 import pandas as pd
 import coinbasepro
 
+#TODO setup connections
+
+c = coinbasepro.PublicClient()
+
 #TODO get necessary balances
+
+class Balance:
+    def __init__(self, ticker = 'ETH-USD'):
+        self.ticker = ticker
+    def open(self):
+        return float(c.get_product_24hr_stats(self.ticker)['open'])
+
 
 #TODO execute re-balance
 
